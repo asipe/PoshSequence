094b6c44eba1fbae5175802461a7cb7a88cbb82f
asipe <ajs.general@gmail.com>
2017-01-14 13:43:47 -0500

    added another excluded rule...

--- analyze.ps1
+++ analyze.ps1
@@ -4,5 +4,6 @@ $ErrorActionPreference = 'Stop'
 Import-Module .\thirdparty\psscriptanalyzer -Force
 
 $excludedRules = @('PSUseShouldProcessForStateChangingFunctions',
-                   'PSUseOutputTypeCorrectly')
+                   'PSUseOutputTypeCorrectly',
+                   'PSUseSingularNouns')
 Invoke-ScriptAnalyzer -Path .\src\PoshSequence -Recurse -ExcludeRule $excludedRules
