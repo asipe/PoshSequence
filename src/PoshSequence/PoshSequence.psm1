7b83cb48b3a699ff574f8114d35fcaceca65df2d
asipe <ajs.general@gmail.com>
2017-01-16 12:54:51 -0500

    updated to functions are exported by manifest

--- src/PoshSequence/PoshSequence.psm1
+++ src/PoshSequence/PoshSequence.psm1
@@ -7,5 +7,4 @@ Get-ChildItem -Path $privateDir -File -Recurse |
   ForEach-Object { . $_.FullName }
 
 Get-ChildItem -Path $publicDir -File -Recurse | 
-  ForEach-Object { . $_.FullName } | 
-  ForEach-Object { Export-ModuleMember -Function $_.BaseName }
+  ForEach-Object { . $_.FullName }
