9702810f1254eef52f45076abb1a749073b67576
asipe <ajs.general@gmail.com>
2017-03-12 08:46:54 -0400

    minor refactoring to reduce branching

--- src/PoshSequence/public/Get-SQNextSequenceValue.ps1
+++ src/PoshSequence/public/Get-SQNextSequenceValue.ps1
@@ -5,12 +5,11 @@ Function Get-SQNextSequenceValue {
   $sequences = $script:sqsequences
 
   if (!($sequences.ContainsKey($SequenceName))) {
-    $sequences.Add($SequenceName, 0)
-    0 | Write-Output
-  } else {
-    $value = $sequences[$SequenceName]
-    ++$value
-    $sequences[$SequenceName] = $value
-    $value | Write-Output
+    $sequences.Add($SequenceName, -1)
   }
+
+  $value = $sequences[$SequenceName]
+  ++$value
+  $sequences[$SequenceName] = $value
+  $value | Write-Output
 }
